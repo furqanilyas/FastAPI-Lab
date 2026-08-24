ace63b574306222280792253863bbbc9ec0a2d42
Furqan Ilyas <furqanilyas2005@gmail.com>
2026-08-24 23:51:13 +0500

    Add product and price lookup

--- lessons/basics.py
+++ lessons/basics.py
@@ -1,15 +1,39 @@
 from fastapi import FastAPI
 from models import Product
+
 app = FastAPI()
+
 
 @app.get("/")
 def greet():
     return "Hello World"
 
-product1 = Product(name = "Laptop", id=1, description="Gaming laptop",
+
+product1 = Product(name="Laptop", id=1, description="Gaming laptop",
                    price=100, quantity=5)
-product2 = Product(name = "Mobile", id=2, description="Usage Mobiles",
+product2 = Product(name="Mobile", id=2, description="Usage Mobiles",
                    price=50, quantity=10)
+product3 = Product(name="Asus", id=3, description="Asus Work",
+                   price=200, quantity=2)
+product4 = Product(name="Machine", id=4, description="Washing Machine",
+                   price=150, quantity=4)
+product_list = [product1, product2, product3, product4]
+
+
 @app.get("/products")
 def products():
-    return product1, product2
+    return product_list
+
+@app.get("/product/{id}")
+def product_by_id(id: int):
+    for item in product_list:
+        if item.id == id:
+            return item
+    return "Product not found"
+
+@app.get("/product/{id}/price")
+def get_price(id: int):
+    for item in product_list:
+        if item.id == id:
+            return {"id": item.id, "price": item.price}
+    return "No product found"
